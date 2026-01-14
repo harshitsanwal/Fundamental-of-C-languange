1d380ec72e0c2027b99e99ecd6f9b5ebb9f3a159
Harshit Sanwal <harshitsanwal2007@gmail.com>
2026-01-14 17:05:34 +0530

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,9 +1,1 @@
-//program to print hello world
-#include<stdio.h> //# is preprocesor directive
-int main()
-{
-printf("hello world\n");//'\n' is use to move the cursor in the next line 
-printf("first c language program\n");
-
-return 0;
-}    
+    
